6f29d6e9446ec6fcc080423256c72a18b3637c81
Gary Wen <Garywn123@gmail.com>
2026-06-05 14:01:26 -0500

    Added environment variables

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ntphopbtsuqbtbmprcsu.supabase.co/";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50cGhvcGJ0c3VxYnRibXByY3N1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MDA2ODc5MiwiZXhwIjoyMDk1NjQ0NzkyfQ.Xm_e0AANQeVzywemdFQQimGm8LkxDY7d0pDA0niKw6A";
-
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
